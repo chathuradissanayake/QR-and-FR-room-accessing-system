d80e674a30595ecbabd71549edacc8fc6e3d7ef8
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-10-15 12:32:20 +0530

    Refactor App component and add SignIn component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import SignIn from './components/SignIn'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+         <SignIn/>
     </>
   )
 }

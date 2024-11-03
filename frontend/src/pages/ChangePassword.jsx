e9bdaccfd0f0ad7eb0f08aa8aa12962d09d70598
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-11-03 15:24:56 +0530

    Re-make the Profile, Change useename, Change password with the Application Design

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { FaChevronLeft, FaEye, FaEyeSlash } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { GoChevronLeft } from "react-icons/go";
+import { Link, useNavigate } from "react-router-dom";
 
 const ChangePassword = () => {
   const [oldPassword, setOldPassword] = useState("");
@@ -27,21 +28,16 @@ const ChangePassword = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 p-4">
-      <div className="max-w-lg mx-auto">
-        {/* Header */}
-        <div className="flex items-center mb-8">
-          <FaChevronLeft
-            className="text-gray-600 cursor-pointer"
-            onClick={handleBackNavigation}
-          />
-          <h1
-            className="ml-2 text-xl font-semibold text-gray-800 cursor-pointer"
-            onClick={handleBackNavigation}
-          >
-            Change Password
-          </h1>
-        </div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+    <div className="bg-white shadow-md rounded-md p-8 w-full max-w-md">
+
+    
+    <div className="title flex items-center space-x-2 mb-8">
+    <Link to="/profile">
+        <GoChevronLeft className="cursor-pointer" />
+    </Link>
+        <span className='font-semibold'>Change Password</span>
+    </div>
 
         {/* Form */}
         <form onSubmit={handleSubmit} className="space-y-6 ml-6">

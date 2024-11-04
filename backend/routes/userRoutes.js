6cfea56bbbc36b6a9df57260a9e9004f093bcdc1
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-04 23:54:07 +0530

    Add endpoint to update user password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const requireAuth = require('../middleware/authMiddleware');
 const { updateUserName } = require('../controllers/userController');
-
+const {updateUserPassword} = require('../controllers/userController')
 
 // Endpoint to update user name
 router.put('/change-username', requireAuth, updateUserName);
+
+// Endpoint to update user password
+router.put('/change-password', requireAuth, updateUserPassword);
 
 module.exports = router;

d675efcea4e224a22bd7a1fe87dd0edf62196ff7
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-04 23:54:02 +0530

    Add password update functionality with validation and error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const {comparePassword, hashPassword } = require('../helper/auth')
 
 // Update user first name and last name
 const updateUserName = async (req, res) => {
@@ -15,7 +16,39 @@ const updateUserName = async (req, res) => {
   }
 };
 
+// Update user password
+const updateUserPassword = async (req, res) => {
+  try {
+    const userId = req.user._id; // Use the user._id from the request object
+    const { oldPassword, newPassword } = req.body;
+
+    // Find the user by ID
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Compare the old password
+    const isMatch = await comparePassword(oldPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Old password is incorrect' });
+    }
+
+    // Hash the new password
+    const hashedPassword = await hashPassword(newPassword);
+
+    // Update the user's password
+    user.password = hashedPassword;
+    await user.save();
+
+    res.json({ message: 'Password updated successfully' });
+  } catch (error) {
+    console.error(error); // Log the error to the console
+    res.status(500).json({ error: 'Error updating password' });
+  }
+};
 
 module.exports = {
   updateUserName,
+  updateUserPassword,
 };

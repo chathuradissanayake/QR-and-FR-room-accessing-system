3ac2ca730ba6e5702c20122fa33978d8fa3eb16b
IT20272654 <shavindurajapaksha@gmail.com>
2024-11-19 01:21:18 +0530

    Updated AskPermission jsx

--- frontend/src/pages/AskPermission.jsx
+++ frontend/src/pages/AskPermission.jsx
@@ -1,48 +1,72 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 import { GoChevronLeft } from "react-icons/go";
-import { Link, useNavigate } from 'react-router-dom';
-
 
 
 const AskPermission = () => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [door, setDoor] = useState('');
-    const [date, setDate] = useState('');
-    const [intime, setinTime] = useState('');
-    const [outtime, setoutTime] = useState('');
-    const [message, setMessage] = useState('');
-    
-    const navigate = useNavigate();
-    
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Handle sign-in logic here
-      console.log('Name:', name);
-      console.log('Room:', room);
-      console.log('Door:', door);
-      console.log('Date:', date);
-      console.log('InTime:', intime);
-      console.log('OutTime:', outtime);
-      console.log('Message:', message);
-      
-      // Clear input values
-      setName('');
-      setRoom('');
-      setDoor('');
-      setDate('');
-      setinTime('');
-      setoutTime('');
-      setMessage('');
+  const [data, setData] = useState({
+    name: '',
+    room: '',
+    door: '',
+    date: '',
+    intime: '',
+    outtime: '',
+    message: '',
+  });
+  const navigate = useNavigate();
 
-      navigate('/success');
-    };
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log('Name:', data.name);
+    console.log('room:', data.room);
+    console.log('door:', data.door);
+    console.log('date:', data.date);
+    console.log('date:', data.intime);
+    console.log('date:', data.outtime);
+    console.log('message:', data.message);
+
+    // Destructuring data
+    const { name, room, door, date, intime, outtime, message } = data;
+
+    try {
+      const { data: response } = await axios.post('/askpermission', {
+        name,
+        room,
+        door,
+        date,
+        intime,
+        outtime,
+        message,
+      });
+      if (response.error) {
+        toast.error(response.error);
+      } else {
+        setData({
+          name: '',
+          room: '',
+          door: '',
+          date: '',
+          intime: '',
+          outtime: '',
+          message: '',
+        });
+        toast.success('Permission sent successfully');
+        navigate('/success');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-
-<>
-    <div className="flex  justify-center min-h-screen bg-gray-50">
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
     <div className="bg-white shadow-md rounded-md p-8 w-full max-w-md">
 
     
@@ -53,7 +77,10 @@ const AskPermission = () => {
         <span className='font-semibold'>Ask Permission</span>
     </div>
     <div className='ml-4'>
-    <form onSubmit={handleSubmit} className="space-y-4">
+
+        {/* Form */}
+        <form onSubmit={handleSubmit} className="space-y-4">
+
           <div>
             <label htmlFor="name" className="sr-only">
               Name
@@ -61,9 +88,10 @@ const AskPermission = () => {
             <input
               type="text"
               id="name"
+              name="name"
               placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={data.name}
+              onChange={handleChange}
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               required
             />
@@ -75,26 +103,31 @@ const AskPermission = () => {
                     Room
                 </label>
                 <select
+                    type="text"
                     id="room"
-                    value={room}
-                    onChange={(e) => setRoom(e.target.value)}
-                    className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${room === '' ? 'text-gray-400' : 'text-black'}`}                >
+                    name="room"
+                    value={data.room}
+                    onChange={handleChange}
+                    className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${data.room === '' ? 'text-gray-400' : 'text-black'}`}                >
                     <option disabled value="">Select the room</option>
                     <option value="room1">Room 1</option>
                     <option value="room2">Room 2</option>
                     <option value="room3">Room 3</option>
                 </select>
             </div>
+      
 
-            <div>
+          <div>
                 <label htmlFor="door" className="sr-only">
                     Door
                 </label>
                 <select
+                    type="text"
                     id="door"
-                    value={door}
-                    onChange={(e) => setDoor(e.target.value)}
-                    className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${door === '' ? 'text-gray-400' : 'text-black'}`}                >
+                    name="door"
+                    value={data.door}
+                    onChange={handleChange}
+                    className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${data.door === '' ? 'text-gray-400' : 'text-black'}`}                >
                     <option disabled value="">Select the Door</option>
                     <option value="door1">Door A</option>
                     <option value="door2">Door B</option>
@@ -102,80 +135,96 @@ const AskPermission = () => {
                 </select>
             </div>
 
+
           <div>
-            <label htmlFor="date" className="sr-only">
-                Date
+          <label htmlFor="date" className="block text-sm text-gray-400 ml-3">
+            Date
             </label>
             <input
                 type="date"
                 id="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${date === '' ? 'text-gray-400' : 'text-black'}`}                
+              name="date"
+              value={data.date}
+              onChange={handleChange}
+                className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${data.date === '' ? 'text-gray-400' : 'text-black'}`}                
                 required
                 // className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
         </div>
 
-
-
         <div>
-            <label htmlFor="intime" className="block text-sm text-gray-400 ml-3">
+        <label htmlFor="intime" className="block text-sm text-gray-400 ml-3">
             In Time
             </label>
             <input
                 type="time"
                 id="intime"
-                value={intime}
-                onChange={(e) => setinTime(e.target.value)}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              name="intime"
+              value={data.intime}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"             
                 required
+                // className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
         </div>
 
         <div>
-            <label htmlFor="intime" className="block text-sm text-gray-400 ml-3">
+        <label htmlFor="intime" className="block text-sm text-gray-400 ml-3">
             Out Time
             </label>
             <input
                 type="time"
                 id="outtime"
-                value={outtime}
-                onChange={(e) => setoutTime(e.target.value)}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 "
+              name="outtime"
+              value={data.outtime}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 "               
+                required
+                // className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
         </div>
 
-        <div>
+          <div>
             <label htmlFor="message" className="sr-only">
               Name
             </label>
             <textarea
               type="text"
               id="message"
+              name="message"
               placeholder="Message"
-              value={message}
+              value={data.message}
+              onChange={handleChange}
               rows="3"
-              onChange={(e) => setMessage(e.target.value)}
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               required
             />
           </div>
 
-        <button
-              onClick={() => navigate()}
+
+          <div>
+            <button
               type="submit"
-              className="w-full py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-300  mb-2"
-          >
-              Submit
-          </button>
-    </form>
+              className="w-full py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-300"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+        </div>
     </div>
 
-    </div>
-    </div>
-    </>
-  )
-  
-}
+
+
+
+
+
+</div>
+  );
+};
+
 export default AskPermission;
+
+
+
+

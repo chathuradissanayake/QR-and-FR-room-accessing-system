cc8069db1354b918539aa52c57a1ba2c665bd799
IT20272654 <shavindurajapaksha@gmail.com>
2024-11-22 12:44:25 +0530

    Removed leaveRoute

--- backend/routes/leaveRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/leaveRoutes.js
-const express = require('express');
-const router = express.Router();
-const { confirmLeave } = require('../controllers/leaveController'); // Import the controller function
-
-// Route to mark leave
-router.post('/mark', confirmLeave); // Use the controller function to handle the POST request
-
-module.exports = router;

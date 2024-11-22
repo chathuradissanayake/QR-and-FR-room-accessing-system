757863fb8b6c24e1cca92bada4a0905953b62197
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-23 01:55:50 +0530

    Refactor authRoutes to remove unused routes and dependencies

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const {loginUser, getProfile, getContactUs } = require('../controllers/authController')
-const {AskPermission, getPermissions } = require('../controllers/askPermissionController');
+const {loginUser, getProfile } = require('../controllers/authController')
 const requireAuth = require('../middleware/authMiddleware');
 
-// middleware
-router.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
 
 // Public routes
 router.post('/signin', loginUser);
 
 // Protected routes
 router.get('/profile', requireAuth, getProfile);
-router.post('/askpermission',requireAuth, AskPermission)
-router.post('/contactus',requireAuth, getContactUs)
-router.get('/permissions',requireAuth, getPermissions);
 
 module.exports = router;

e1eb90f862a598892d594ef6cbfcc97b9170f676
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-11-23 16:07:14 +0530

    Add doorAccess and history fields to user schema for tracking access and requests

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,29 +2,25 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  userId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  doorAccess: [{
+    door: { type: Schema.Types.ObjectId, ref: 'Door' },
+    inTime: { type: String },
+    outTime: { type: String },
+    date: { type: Date }
+  }],
+  pendingRequests: [{ type: Schema.Types.ObjectId, ref: 'PermissionRequest' }],
+  history: [{
+    door: { type: Schema.Types.ObjectId, ref: 'Door' },
+    entryTime: { type: Date },
+    exitTime: { type: Date },
+    status: { type: String, enum: ['Active', 'Exited'], default: 'Active' },
+  }],
+}, { timestamps: true });
 
 const UserModel = mongoose.model('User', userSchema);
 

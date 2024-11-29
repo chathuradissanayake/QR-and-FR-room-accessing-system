bddca4dea7c5de0a42eecab64a317ade6132cf53
IT20272654 <shavindurajapaksha@gmail.com>
2024-11-29 13:02:51 +0530

    Created accessHistoryRoutes

---
+++ backend/models/accessHistory.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const timeSchema = new mongoose.Schema({
+    
+  entryTime: { type: Date },
+  exitTime: { type: Date },
+});
+
+const Time = mongoose.model('Time', timeSchema);
+
+module.exports = Time;

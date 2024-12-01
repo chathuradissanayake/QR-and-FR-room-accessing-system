4a504d9e8f4fd224e572cf1216414d0c12c81cbc
IT20272654 <shavindurajapaksha@gmail.com>
2024-12-02 01:32:11 +0530

    Created Model History

---
+++ backend/models/History.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const historySchema = new Schema({
+  doorCode: { type: String, required: true },
+  entryTime: { type: Date, required: true },
+  location: { type: String},
+  roomName: { type: String},
+  user: {
+    userId: { type: String, required: true },
+  },
+});
+
+const History = mongoose.model("History", historySchema);
+
+module.exports = History;

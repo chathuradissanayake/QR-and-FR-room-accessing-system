05c03a4653e55cf54e84b7e5afc4d1ec4073aa46
Yoosuf Aathil <yoosufaath007@gmail.com>
2025-01-03 23:37:36 +0530

    Add company reference to PermissionRequest model

--- backend/models/PermissionRequest.js
+++ backend/models/PermissionRequest.js
@@ -1,52 +1,63 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const permissionRequestSchema = new Schema({
-  user: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true 
+const permissionRequestSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    company: { 
+      type: Schema.Types.ObjectId, 
+      ref: "Company", 
+      required: true 
+    }, 
+    door: {
+      type: Schema.Types.ObjectId,
+      ref: "Door",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    roomName: {
+      type: String,
+      required: true,
+    },
+    inTime: {
+      type: String,
+      required: true,
+    },
+    outTime: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    requestTime: {
+      type: Date,
+      default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
   },
-  door: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'Door', 
-    required: true 
-  },
-  name: { 
-    type: String, 
-    required: true 
-  },
-  roomName: { 
-    type: String, 
-    required: true 
-  },
-  inTime: { 
-    type: String, 
-    required: true 
-  }, 
-  outTime: { 
-    type: String, 
-    required: true 
-  }, 
-  date: { 
-    type: Date, 
-    required: true 
-  }, 
-  message: { 
-    type: String, 
-    required: true 
-  },
-  requestTime: { 
-    type: Date, 
-    default: Date.now 
-  },
-  status: { 
-    type: String, 
-    enum: ['Pending', 'Approved', 'Rejected'], 
-    default: 'Pending' 
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-const PermissionRequest = mongoose.model('PermissionRequest', permissionRequestSchema);
+const PermissionRequest = mongoose.model(
+  "PermissionRequest",
+  permissionRequestSchema
+);
 
 module.exports = PermissionRequest;
